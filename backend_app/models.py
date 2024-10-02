c5ec53121abbe11623e2d54d71ebe7ca8d81fab0
frank@gmail.com <imafidonfrank2015@gmail.com>
2024-10-02 08:46:24 +0100

    first commit

--- backend_app/models.py
+++ backend_app/models.py
@@ -21,8 +21,10 @@ class Register(models.Model):
     
 class UploadedFiles(models.Model):
     fileowner = models.CharField(max_length=255)
+    filereceiver = models.CharField(max_length=255, default='')
     uploaded_file = models.TextField()
     file_date = models.DateField(auto_now_add=True)
+
 
     
     def __str__(self):
