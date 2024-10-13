80b0b85510462c1c7ab2a4109f86372b08742a69
frank@gmail.com <imafidonfrank2015@gmail.com>
2024-10-13 07:14:35 +0100

    first commit

--- backend_app/views.py
+++ backend_app/views.py
@@ -40,6 +40,8 @@ class Form1View(viewsets.ModelViewSet):
     serializer_class = Form1Serializer
     renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
     template_name = 'form1.html'
+    queryset = Form1.objects.all()
+
 
    
     def get(self, request, *args, **kwargs):
