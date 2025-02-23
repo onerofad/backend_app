8f272db3e7512bb9ccdef861174844dd1a56a968
frank@gmail.com <imafidonfrank2015@gmail.com>
2025-02-23 13:25:28 +0100

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y4a_1olot4oezni85ek%$3gh-ne@l#fx^az-@1*t^tv4-v3kpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost', 'https://masta-plana.vercel.app', 'https://courseweb-app.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost', 'https://mastaplana-web.vercel.app', 'https://courseweb-app.vercel.app']
 
 
 # Application definition
@@ -151,7 +151,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'https://masta-plana.vercel.app',
+    'https://mastaplana-web.vercel.app',
     'https://courseweb-app.vercel.app'
 ]
 
